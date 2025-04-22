e78c5c8386b257effd1584ddff06ca8e61bb0cb8
TH4SL4M <124847835+TH4SL4M@users.noreply.github.com>
2025-04-22 15:17:11 -0600

    Update install.R

--- .binder/install.R
+++ .binder/install.R
@@ -1,12 +1,10 @@
 options(repos = c(CRAN = "https://packagemanager.posit.co/cran/2021-01-01"))
 
-# Core packages for CourseKata environment
 pkgs <- c(
   "IRkernel", "mosaic", "supernova", "Lock5withR", 
   "fivethirtyeight", "lsr", "repr", "remotes"
 )
 
-# Install from CRAN if not already installed
 install_if_missing <- function(pkg) {
   if (!requireNamespace(pkg, quietly = TRUE)) {
     install.packages(pkg)
@@ -15,9 +13,5 @@ install_if_missing <- function(pkg) {
 
 invisible(lapply(pkgs, install_if_missing))
 
-# Install coursekata from a known-working commit (early 2022)
+# Install coursekata from an older version that works with R 4.0.3
 remotes::install_github("CourseKata/coursekata-r@7f9b1fc")
-
-# Register IRkernel and set Deepnote-friendly plot defaults
-IRkernel::installspec()
-cat('options(repr.plot.width = 4, repr.plot.height = 4)\n', file = "~/.Rprofile", append = TRUE)
