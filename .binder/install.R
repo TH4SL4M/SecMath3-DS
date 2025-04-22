127b126f198542864e9bc2f5f25418571aebd778
TH4SL4M <124847835+TH4SL4M@users.noreply.github.com>
2025-04-22 14:32:07 -0600

    Update install.R

--- .binder/install.R
+++ .binder/install.R
@@ -1,12 +1,18 @@
 install.packages("remotes")
-remotes::install_github("r-lib/gtable@v0.3.4")  # Needed for ggplot2 >= 3.4
-remotes::install_github("tidyverse/ggplot2@v3.4.4")  # latest safe version for R 4.2.1
 
+# Force upgrade of rlang first
+remotes::install_cran("rlang", upgrade = "always")
+
+# Then install gtable & ggplot2 from GitHub
+remotes::install_github("r-lib/gtable@v0.3.4")
+remotes::install_github("tidyverse/ggplot2@v3.4.4")
+
+# Install IRkernel
 install.packages("IRkernel")
 IRkernel::installspec(user = FALSE)
 
-# Install tidyverse AFTER ggplot2 so it doesn't downgrade ggplot2
+# Now install tidyverse without downgrading ggplot2
 install.packages("tidyverse")
 
-# Finally install CourseKata
+# Then finally install coursekata
 remotes::install_github("CourseKata/coursekata-r")
