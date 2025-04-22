21b6b8222cb59da0494ffdd2aa3b92d1cd28a495
TH4SL4M <124847835+TH4SL4M@users.noreply.github.com>
2025-04-22 12:57:56 -0600

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,4 @@
-install.packages("coursekata")
+install.packages("remotes")
+remotes::install_github("IRkernel/IRkernel")
+remotes::install_github("coursekata/coursekata-r")
+IRkernel::installspec(user = FALSE)  # ← This registers the R kernel with Jupyter
