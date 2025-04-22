8d2cd395716212dc332bbb52063244e5118c9bb3
TH4SL4M <124847835+TH4SL4M@users.noreply.github.com>
2025-04-22 15:33:54 -0600

    Update install.R

--- .binder/install.R
+++ .binder/install.R
@@ -1,17 +1,9 @@
-options(repos = c(CRAN = "https://cran.r-project.org"))
+install.packages("IRkernel")
+IRkernel::installspec(user = FALSE)
 
-pkgs <- c(
-  "IRkernel", "mosaic", "supernova", "Lock5withR", 
-  "fivethirtyeight", "lsr", "repr", "remotes"
-)
+install.packages("remotes")
+library(remotes)
 
-install_if_missing <- function(pkg) {
-  if (!requireNamespace(pkg, quietly = TRUE)) {
-    install.packages(pkg)
-  }
-}
 
-invisible(lapply(pkgs, install_if_missing))
-
-# Install CourseKata using known good commit (can be updated later)
-remotes::install_github("CourseKata/coursekata-r@7f9b1fc")
+# Then install coursekata
+remotes::install_github("CourseKata/coursekata-r")
