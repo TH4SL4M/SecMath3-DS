e498e7c7322142bd47f96835769cba6e805876cc
TH4SL4M <124847835+TH4SL4M@users.noreply.github.com>
2025-04-22 14:09:09 -0600

    Update install.R

--- .binder/install.R
+++ .binder/install.R
@@ -2,8 +2,13 @@ install.packages("IRkernel")
 IRkernel::installspec(user = FALSE)
 
 install.packages("remotes")
-library(remotes)  # <-- add this line to ensure it's available
+library(remotes)
 
+# install a modern ggplot2 version from GitHub first
+remotes::install_github("tidyverse/ggplot2")
 
-install.packages(c("tidyverse", "ggplot2", "dplyr"))
-install_github("CourseKata/coursekata-r")
+# Now install the coursekata package
+remotes::install_github("CourseKata/coursekata-r")
+
+# other packages you need
+install.packages(c("tidyverse", "dplyr"))
