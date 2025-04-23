40c39d1760a10bd49a9193bf198e01f8c82e9cda
TH4SL4M <124847835+TH4SL4M@users.noreply.github.com>
2025-04-23 06:31:31 -0600

    Update install.R

--- .binder/install.R
+++ .binder/install.R
@@ -1,9 +1,4 @@
 install.packages("IRkernel")
 IRkernel::installspec(user = FALSE)
 
-install.packages("remotes")
-library(remotes)
-
-
-# Then install coursekata
-remotes::install_github("CourseKata/coursekata-r")
+install.packages("coursekata")
