af0d9af44bc871842551d89a780801967da331b7
TH4SL4M <124847835+TH4SL4M@users.noreply.github.com>
2025-04-23 18:27:08 -0600

    Update install.R

--- .binder/install.R
+++ .binder/install.R
@@ -1,6 +1,9 @@
 install.packages("IRkernel")
-install.packages("devtools")  # Needed to install GitHub packages
-
+install.packages("devtools")
 IRkernel::installspec(user = FALSE)
 
+# Install compatible version of ggplot2 (3.4.0 or later)
+install.packages("ggplot2", repos = "https://cloud.r-project.org", dependencies = TRUE)
+
+# THEN install coursekata
 devtools::install_github("CourseKata/coursekata-r")
